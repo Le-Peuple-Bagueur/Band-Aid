7ecd1a723fe398b605500df7e5d888f030aaaffc
François Bolduc <drpacoqc@gmail.com>
2026-02-11 13:28:36 -0500

    bat file starts minimized, vbs also works

--- run_app.bat
+++ run_app.bat
@@ -1,6 +1,14 @@
 @echo off
 setlocal
 
+REM --------------------------------------------
+REM Relaunch this batch minimized on first run
+REM --------------------------------------------
+if /i not "%~1"=="min" (
+  start "" /min cmd /c "%~f0" min
+  exit /b
+)
+
 REM Go to folder where this BAT file lives
 cd /d "%~dp0"
 
---
+++ start_app.vbs
@@ -0,0 +1,3 @@
+Set sh = CreateObject("WScript.Shell")
+' 7 = minimized, 0 = hidden; False = do not wait
+sh.Run "C:\Users\BolducF\Documents\ShinyApps\Band-Aid\run_app.bat", 7, False
